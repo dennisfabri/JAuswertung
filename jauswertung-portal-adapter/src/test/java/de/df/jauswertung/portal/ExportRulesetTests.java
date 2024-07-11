d950f677a4d3bb73baaca148270eae924a9d33f0
Dennis Fabri <info@dennisfabri.de>
2024-07-11 07:48:08 +0200

    Export aktualisiert

--- jauswertung-portal-adapter/src/test/java/de/df/jauswertung/portal/ExportRulesetTests.java
+++ jauswertung-portal-adapter/src/test/java/de/df/jauswertung/portal/ExportRulesetTests.java
@@ -5,41 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.lisasp.competition.registration.JsonObjectMapper;
 import org.lisasp.competition.registration.domain.ruleset.templates.RulesetTemplate;
 
 public class ExportRulesetTests {
-    @Test
-    void einzel2024Test() throws IOException {
+    @ParameterizedTest
+    @ValueSource(strings = { "DLRG 2024.rwe", "DLRG 2024.rwm", "International - Ocean.rwe", "International - Ocean.rwm",
+            "International - Ocean Mixed.rwm", "International - Pool.rwe", "International - Pool.rwm",
+            "International - Pool Mixed.rwm" })
+    void einzel2024Test(String source) throws IOException {
         ExportRuleset exportRuleset = new ExportRuleset();
-        String filename = "./src/test/resources/rulesets/DLRG 2024.rwe";
+        String filename = "./src/test/resources/rulesets/" + source;
         RulesetTemplate actual = exportRuleset.toRuleSetTemplate(filename);
         RulesetTemplate expected = JsonObjectMapper.getInstance()
-                                                   .readValue(Path.of(
-                                                                          "./src/test/resources/rulesets/DLRG-2024-Individual.json")
-                                                                  .toUri()
-                                                                  .toURL(), RulesetTemplate.class);
+                .readValue(Path.of(
+                        "./src/test/resources/rulesets/" + source + ".json")
+                        .toUri()
+                        .toURL(), RulesetTemplate.class);
         System.out.println(JsonObjectMapper.getInstance().writeValueAsString(actual));
         assertEquals(JsonObjectMapper.getInstance().writerWithDefaultPrettyPrinter().writeValueAsString(expected),
-                     JsonObjectMapper.getInstance().writerWithDefaultPrettyPrinter().writeValueAsString(actual));
+                JsonObjectMapper.getInstance().writerWithDefaultPrettyPrinter().writeValueAsString(actual));
         assertEquals(expected, actual);
     }
-
-    @Test
-    void mannschaft2024Test() throws IOException {
-        ExportRuleset exportRuleset = new ExportRuleset();
-        String filename = "./src/test/resources/rulesets/DLRG 2024.rwm";
-        RulesetTemplate actual = exportRuleset.toRuleSetTemplate(filename);
-        RulesetTemplate expected = JsonObjectMapper.getInstance()
-                                                   .readValue(Path.of(
-                                                                          "./src/test/resources/rulesets/DLRG-2024-Team.json")
-                                                                  .toUri()
-                                                                  .toURL(), RulesetTemplate.class);
-        System.out.println(JsonObjectMapper.getInstance().writeValueAsString(actual));
-        assertEquals(JsonObjectMapper.getInstance().writerWithDefaultPrettyPrinter().writeValueAsString(expected),
-                     JsonObjectMapper.getInstance().writerWithDefaultPrettyPrinter().writeValueAsString(actual));
-        assertEquals(expected, actual);
-    }
-
 }
