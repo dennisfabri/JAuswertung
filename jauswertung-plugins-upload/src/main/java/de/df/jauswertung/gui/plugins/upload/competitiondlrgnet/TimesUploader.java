8dbd935eca59057209b5812ee453bbd6b7bd8a50
Dennis Fabri <info@dennisfabri.de>
2026-06-27 12:23:47 +0200

    Improve upload

--- jauswertung-plugins-upload/src/main/java/de/df/jauswertung/gui/plugins/upload/competitiondlrgnet/TimesUploader.java
+++ jauswertung-plugins-upload/src/main/java/de/df/jauswertung/gui/plugins/upload/competitiondlrgnet/TimesUploader.java
@@ -1,5 +1,6 @@
 package de.df.jauswertung.gui.plugins.upload.competitiondlrgnet;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.df.jauswertung.daten.AWettkampf;
 import de.df.jauswertung.daten.PropertyConstants;
@@ -12,8 +13,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.Duration;
 
 public class TimesUploader {
@@ -48,45 +47,54 @@ public class TimesUploader {
             return;
         }
         try {
-            ObjectMapper mapper = new ObjectMapper();
-
-            String body = mapper.writeValueAsString(results);
-
-            if (nothingChanged(uploadId, index, body)) {
-                log.info("No changes detected. Skipping upload.");
-                return;
+            String body = toJson(results);
+            if (hasChanges(uploadId, index, body)) {
+                uploadJson(uploadId, index, body);
+                markAsUploaded(uploadId, index, body);
             }
-            lastUploadId = uploadId;
-            lastUploadIndex = index;
-            lastUploadedJson = body;
+        } catch (JsonProcessingException e) {
+            log.error("Error converting results to JSON: {}", e.getMessage(), e);
+        } catch (IOException ex) {
+            log.error("Error uploading results to web: {}", ex.getMessage(), ex);
+        }
+    }
 
+    private void markAsUploaded(String uploadId, int index, String json) {
+        lastUploadId = uploadId;
+        lastUploadIndex = index;
+        lastUploadedJson = json;
+    }
+
+    private boolean hasChanges(String uploadId, int index, String body) {
+        if (nothingChanged(uploadId, index, body)) {
+            log.info("No changes detected. Skipping upload.");
+            return false;
+        }
+        return true;
+    }
+
+    private void uploadJson(String uploadId, int index, String body) throws IOException {
+        try {
             log.info("Uploading results to web.");
             // log.info(body);
             //Files.writeString(Path.of(uploadId + "-" + index + ".json"), body);
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(String.join("/",
-                            new String[] {
-                                    serverUrl,
-                                    "rest", "result", "import",
-                                    "jauswertung", uploadId, "" + index
-                            })))
-                    .timeout(Duration.ofMinutes(2))
-                    .header("Content-Type", "application/json")
-                    .PUT(HttpRequest.BodyPublishers.ofString(body))
-                    .build();
-            try (
-                    HttpClient client = HttpClient.newBuilder()
-                            .version(HttpClient.Version.HTTP_1_1)
-                            .followRedirects(HttpClient.Redirect.NORMAL)
-                            .connectTimeout(Duration.ofSeconds(20))
-                            .build()) {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(String.join("/",
+                                                                                      new String[]{serverUrl, "rest", "result", "import", "jauswertung", uploadId, "" + index}))).timeout(
+                    Duration.ofMinutes(2)).header("Content-Type", "application/json").PUT(HttpRequest.BodyPublishers.ofString(body)).build();
+            try (HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).followRedirects(HttpClient.Redirect.NORMAL).connectTimeout(
+                    Duration.ofSeconds(20)).build()) {
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 System.out.println(response.statusCode());
                 System.out.println(response.body());
             }
-        } catch (IOException | InterruptedException iox) {
-            iox.printStackTrace();
+        } catch (InterruptedException ix) {
+            Thread.currentThread().interrupt();
         }
+    }
+
+    private static String toJson(JAuswertungCompetition results) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(results);
     }
 
     private boolean nothingChanged(String uploadId, int index, String body) {
